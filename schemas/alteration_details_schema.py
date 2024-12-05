4c87abdd039067412fa2decd370089b31540ac84
NeTrioGit <5wngh@naver.com>
2024-12-05 22:03:22 +0900

    feat: 12/05 수정사항 업데이트

--- schemas/alteration_details_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-from typing import Optional
-
-class AlterationDetailsInfo(BaseModel):
-    form_repair_id: int
-    figure: Optional[float] = None
-    alterationFigure: Optional[float] = None
